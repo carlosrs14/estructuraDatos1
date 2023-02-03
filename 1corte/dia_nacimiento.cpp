59fcd5c0b2e632b9ec6ddfd8ca22e2ae226215f5
carlosrs14 <crincones578@gmail.com>
2023-02-03 03:46:28 -0500

    bug reparado

--- 1corte/dia_nacimiento.cpp
+++ 1corte/dia_nacimiento.cpp
@@ -8,7 +8,7 @@ bool isBisiesto(int);
 int calcular(int nacimiento[3], int actual[3], int hoy);
 
 int main(){
-    int fecha_ac[3], fecha_na[3], diaint, dia_hoy=5;
+    int fecha_na[3], fecha_ac[3], diaint, dia_hoy=5;
 
     cout<<"Lunes\t\t1\nmartes\t\t2\nmiercoles\t3\njueves\t\t4\nviernes\t\t5\nsabado\t\t6\ndomingo\t\t7"<<endl;
     cout<<"Ingrese el día de la semana de hoy: ";
@@ -69,9 +69,16 @@ int calcular(int nacimiento[3], int actual[3], int hoy){
     
     for(int i = nacimiento[2]; i <= actual[2]; i++){
         
-        //printf("[%d dias]", dias);
+        
         if(i==nacimiento[2]){
-            
+            if(isBisiesto(i)){
+                if(nacimiento[1]==2 && nacimiento[0]<=28){
+                    dias++;
+                }else if(nacimiento[1]==1){
+                    dias++;
+                }
+            }
+
             for(int j = nacimiento[1]; j<=12; j++){
                 if(j==nacimiento[1]){
                     dias+=meses[j-1]-nacimiento[0]+1;
